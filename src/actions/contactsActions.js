dd18beb4ecc4bc3a3ceac4f1f43fdcac16d4949c
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-02-27 00:43:43 +0600

    react router dom done

--- src/actions/contactsActions.js
+++ src/actions/contactsActions.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-unreachable */
 import { redirect } from "react-router-dom";
-import { createContact, updateContact } from "../contacts";
+import { createContact, deleteContact, updateContact } from "../contacts";
 
 export async function createContactAtion() {
   const contact = await createContact();
-  return { contact };
+  return redirect(`contacts/${contact.id}/edit`);
 }
 
 export async function eidtContactAction({ request, params }) {
@@ -11,4 +12,17 @@ export async function eidtContactAction({ request, params }) {
   const updates = Object.fromEntries(formData);
   await updateContact(params.contactId, updates);
   return redirect(`/contacts/${params.contactId}`);
+}
+
+export async function deleteContactAction({ params }) {
+  throw new Error("oh dang!");
+  await deleteContact(params.contactId);
+  return redirect("/");
+}
+
+export async function updateContactFormAction({ request, params }) {
+  let formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
 }

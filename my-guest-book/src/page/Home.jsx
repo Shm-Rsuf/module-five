ead93b1b430f9ad9a6253dbee89425fbc32058d0
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-03-15 16:09:31 +0600

    user logout added

--- my-guest-book/src/page/Home.jsx
+++ my-guest-book/src/page/Home.jsx
@@ -1,5 +1,35 @@
+import { signOut } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
 const Home = () => {
-  return <div>Home</div>;
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  /* handleSignout */
+  const handleSignout = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/login");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  if (loading) return <p>User info loading..</p>;
+  return (
+    <div>
+      <p>Welcome, {user.email.split("@")[0].toUpperCase()}</p>
+      <button
+        className='mt-5 m-auto px-10 border py-2 rounded-md uppercase tracking-wider font-bold text-gray-500 hover:bg-gradient-to-r from-slate-300 to-slate-700 hover:text-gray-50 duration-500'
+        type='submit'
+        onClick={handleSignout}
+      >
+        SignOut
+      </button>
+    </div>
+  );
 };
 
 export default Home;

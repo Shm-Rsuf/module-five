d030bcbda529f3b76521c6990ee93f4e85d14d6b
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-03-15 10:40:28 +0600

    my-guest-book project setup done

---
+++ my-guest-book/src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className='text-4xl text-cyan-400 font-bold'>App</div>;
+};
+
+export default App;
---
+++ my-guest-book/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_LDo_xRN2lhKddZ-uKaVqA0i3QOvKGX4",
+  authDomain: "my-guest-book-74082.firebaseapp.com",
+  projectId: "my-guest-book-74082",
+  storageBucket: "my-guest-book-74082.appspot.com",
+  messagingSenderId: "799855747807",
+  appId: "1:799855747807:web:29c722410e1af764b47f6a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
